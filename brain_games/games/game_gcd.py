92b731e32681b085e9c207b895a9819d485fdbbd
Nikolay Ulanov <iamnickolasulanov@gmail.com>
2021-01-17 15:45:10 +0300

    fixing linter errors

--- brain_games/games/game_gcd.py
+++ brain_games/games/game_gcd.py
@@ -13,18 +13,18 @@ def start_gcd_game():
         number2 = randint(1, 50)
         print("Question: {}".format(str(number1) + " " + str(number2)))
         answer = prompt.string("Your answer: ")
-        if int(answer) == calculate_gcd(number1, number2):
+        if int(answer) == calc_gcd(number1, number2):
             print("Correct!")
             counter = counter + 1
         else:
-            print("{} is wrong answer ;(. Correct answer was '{}'".format(answer, str(calculate_gcd(number1, number2))))
+            print(f"{answer} is wrong answer ;(. Correct answer was '{calc_gcd(number1,number2)}'")
             print(f"Let's try again, {user_name}!")
             return
     if count(counter):
         print(f"Congratulations, {user_name}!")
 
 
-def calculate_gcd(num1, num2):
+def calc_gcd(num1, num2):
     while num1 != num2:
         if num1 > num2:
             num1 = num1 - num2
