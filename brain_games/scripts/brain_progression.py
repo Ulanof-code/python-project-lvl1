e9530eb8c49bafd99bd65d1e7b0a3a5ca3755bf7
Nikolay Ulanov <iamnickolasulanov@gmail.com>
2021-01-18 18:12:33 +0300

    added script brain_progression.py

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from brain_games.games.game_progression import start_progression_game
+
+
+def main():
+    start_progression_game()
+
+
+if __name__ == '__main__':
+    main()
